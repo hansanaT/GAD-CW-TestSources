ff0982d79896771ed0b8548a92097d751a52feaf
Hansana Adhikari <CODSE223F-096@student.nibm.lk>
2023-07-17 23:16:34 +0530

    Test

---
+++ Send_Mail_Test/Send_Mail.cs
@@ -0,0 +1,80 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using MailKit.Net.Smtp;
+using MimeKit;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Send_Mail_Test
+{
+    public partial class Send_Mail : Form
+    {
+        public Send_Mail()
+        {
+            InitializeComponent();
+        }
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            string emailAddress = txt_email.Text;
+            string messageText = txt_msg.Text;
+
+            try
+            {
+                UserCredential credential;
+
+                using (var stream = new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+                {
+                    string credPath = "token.json";
+                    credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                        GoogleClientSecrets.FromStream(stream).Secrets,
+                        new[] { GmailService.Scope.GmailSend },
+                        "user",
+                        CancellationToken.None,
+                        new FileDataStore(credPath, true)).Result;
+                }
+
+                var gmailService = new GmailService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Your Application Name"
+                });
+
+                var emailMessage = new MimeMessage();
+                emailMessage.From.Add(new MailboxAddress("Hansana", "hansanathimeth@gmail.com")); //Sender's Name and Email Address
+                emailMessage.To.Add(new MailboxAddress("", emailAddress)); // Recipient's email address
+                emailMessage.Subject = "Welcome to the Application!";
+                emailMessage.Body = new TextPart("plain")
+                {
+                    Text = messageText
+                };
+
+                var rawMessage = Base64UrlEncode(emailMessage.ToString());
+                var message = new Google.Apis.Gmail.v1.Data.Message
+                {
+                    Raw = rawMessage
+                };
+
+                gmailService.Users.Messages.Send(message, "me").Execute();
+
+                MessageBox.Show("Email sent successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while sending the email: {ex.Message}");
+            }
+        }
+        private static string Base64UrlEncode(string input)
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            return System.Convert.ToBase64String(inputBytes)
+                .Replace('+', '-')
+                .Replace('/', '_')
+                .Replace("=", "");
+        }
+    }
+}
---
+++ WFA_OAuth/SignUp.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WFA_OAuth
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_signUp = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_signUp
+            // 
+            this.btn_signUp.Location = new System.Drawing.Point(292, 198);
+            this.btn_signUp.Name = "btn_signUp";
+            this.btn_signUp.Size = new System.Drawing.Size(181, 64);
+            this.btn_signUp.TabIndex = 0;
+            this.btn_signUp.Text = "Sign Up With Google";
+            this.btn_signUp.UseVisualStyleBackColor = true;
+            this.btn_signUp.Click += new System.EventHandler(this.btn_signUp_Click);
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_signUp);
+            this.Name = "SignUp";
+            this.Text = "Google Sign Up";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_signUp;
+    }
+}
+
