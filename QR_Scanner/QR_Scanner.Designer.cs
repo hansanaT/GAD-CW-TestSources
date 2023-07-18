067d303e25f0cad131a7485849a1e99d0af7b148
Hansana Adhikari <CODSE223F-096@student.nibm.lk>
2023-07-18 14:15:58 +0530

    Included QR Scanner

---
+++ QR_Scanner/QR_Scanner.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace QR_Scanner
+{
+    partial class QR_Scanner
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pb_qr = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmb_camType = new System.Windows.Forms.ComboBox();
+            this.txt_qr = new System.Windows.Forms.TextBox();
+            this.btn_scan = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_qr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_qr
+            // 
+            this.pb_qr.Location = new System.Drawing.Point(31, 81);
+            this.pb_qr.Name = "pb_qr";
+            this.pb_qr.Size = new System.Drawing.Size(549, 432);
+            this.pb_qr.TabIndex = 0;
+            this.pb_qr.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(246, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Camera";
+            // 
+            // cmb_camType
+            // 
+            this.cmb_camType.FormattingEnabled = true;
+            this.cmb_camType.Location = new System.Drawing.Point(343, 17);
+            this.cmb_camType.Name = "cmb_camType";
+            this.cmb_camType.Size = new System.Drawing.Size(337, 24);
+            this.cmb_camType.TabIndex = 2;
+            // 
+            // txt_qr
+            // 
+            this.txt_qr.Location = new System.Drawing.Point(616, 183);
+            this.txt_qr.Multiline = true;
+            this.txt_qr.Name = "txt_qr";
+            this.txt_qr.Size = new System.Drawing.Size(298, 92);
+            this.txt_qr.TabIndex = 3;
+            // 
+            // btn_scan
+            // 
+            this.btn_scan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_scan.Location = new System.Drawing.Point(708, 281);
+            this.btn_scan.Name = "btn_scan";
+            this.btn_scan.Size = new System.Drawing.Size(113, 41);
+            this.btn_scan.TabIndex = 4;
+            this.btn_scan.Text = "&Scan";
+            this.btn_scan.UseVisualStyleBackColor = true;
+            this.btn_scan.Click += new System.EventHandler(this.btn_scan_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // QR_Scanner
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(961, 525);
+            this.Controls.Add(this.btn_scan);
+            this.Controls.Add(this.txt_qr);
+            this.Controls.Add(this.cmb_camType);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pb_qr);
+            this.Name = "QR_Scanner";
+            this.Text = "Scan";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.QR_Scanner_FormClosing);
+            this.Load += new System.EventHandler(this.QR_Scanner_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_qr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_qr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmb_camType;
+        private System.Windows.Forms.TextBox txt_qr;
+        private System.Windows.Forms.Button btn_scan;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ QR_Scanner/QR_Scanner.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Animation;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace QR_Scanner
+{
+    public partial class QR_Scanner : Form
+    {
+        
+        public QR_Scanner()
+        {
+            InitializeComponent();
+        }
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice captureDevice;
+
+        private void QR_Scanner_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                cmb_camType.Items.Add(filterInfo.Name);
+            }
+            cmb_camType.SelectedIndex = 0;
+        }
+
+        private void btn_scan_Click(object sender, EventArgs e)
+        {
+            captureDevice = new VideoCaptureDevice(filterInfoCollection[cmb_camType.SelectedIndex].MonikerString);
+            captureDevice.NewFrame += CaptureDevice_NewFrame;
+            captureDevice.Start();
+            timer1.Start();
+        }
+
+        private void CaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            pb_qr.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void QR_Scanner_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (captureDevice.IsRunning)
+                captureDevice.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (pb_qr.Image != null)
+            {
+                BarcodeReader barcodeReader = new BarcodeReader();
+                Result result = barcodeReader.Decode((Bitmap)pb_qr.Image);
+                if (result != null)
+                {
+                    txt_qr.Text = result.ToString();
+                    timer1.Stop();
+                    if (captureDevice.IsRunning)
+                        captureDevice.Stop();
+                }
+            }
+        }
+    }
+}
